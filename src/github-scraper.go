6f4f2dd29e25b8b2cabdece17d976a9cd84a8dc6
ibnaleem <ibnaleem@outlook.com>
2026-02-09 12:31:54 +0000

    chore: move UnmarshalStruct to utils.go
    - delete unused imports as well

--- src/github-scraper.go
+++ src/github-scraper.go
@@ -2,14 +2,6 @@
 
 package main
 
-import (
-	"fmt"
-	"log"
-	"io"
-	"net/http"
-	"encoding/json"
-)
-
 type GitHubUser struct {
 	NodeID           string     `json:"node_id,omitempty"`
 	GravatarID	     string 	 `json:"gravatar_id,omitempty"`
@@ -42,67 +34,6 @@ type GitHubFollowing struct {
 }
 
 var mutualFollowers = []string{}
-
-
-
-func UnmarshalStruct[T any](url string, isArray bool) (any, error) {
-    // Since GoSearch unmarshals JSON plenty, we can create a function that returns the type
-	// This prevents repetitive code
-
-	client := http.Client{}
-
-	var zero any
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-		if err != nil {
-			log.Fatal("In function UnmarshalStruct (line 56): ", err)
-			return zero, err
-	}
-	req.Header.Set("User-Agent", DefaultUserAgent)
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Upgrade-Insecure-Requests", "1")
-	req.Header.Set("Sec-Fetch-Dest", "document")
-	req.Header.Set("Sec-Fetch-Mode", "navigate")
-	req.Header.Set("Sec-Fetch-Site", "none")
-	req.Header.Set("Sec-Fetch-User", "?1")
-	req.Header.Set("Cache-Control", "max-age=0")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-		return zero, err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Errorf("failed to fetch API in UnmarshalStruct, line 81, status code: %d", resp.StatusCode)
-		return zero, err
-	}
-
-	JSONData, err := io.ReadAll(resp.Body)
-
-
-	if isArray {
-		var successObj []T
-		err = json.Unmarshal(JSONData, &successObj)
-		if err != nil {
-			return zero, fmt.Errorf("error unmarshalling JSON (array): %w", err)
-		}
-		return successObj, nil
-	} else {
-		var successObj T
-		err = json.Unmarshal(JSONData, &successObj)
-		if err != nil {
-			return zero, fmt.Errorf("error unmarshalling JSON (singular): %w", err)
-		}
-		return successObj, nil
-	}
-
-}
 
 
 func DisplayGitHubInfo(githubUser GitHubUser, username string) {
