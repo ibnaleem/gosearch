6f968bede2efd54dd17785866a3d7addf67da6ae
ibnaleem <ibnaleem@outlook.com>
2026-03-24 19:38:27 +0000

    fix: refactor print statements to display correct info

--- src/github-scraper.go
+++ src/github-scraper.go
@@ -9,26 +9,26 @@ import (
 )
 
 type GitHubUser struct {
-	NodeID           string     `json:"node_id,omitempty"`
-	GravatarID	     string 	 `json:"gravatar_id,omitempty"`
-	AvatarURL        string     `json:"avatar_url,omitempty"`
-	TwitterUsername  string		`json:"twitter_username,omitempty"`
-	Name 		     string 	 `json:"name,omitempty"`
-	Company 	     string 	 `json:"company,omitempty"`
-	Blog             string     `json:"blog,omitempty"`
-	Location         string     `json:"location,omitempty"`
-	Email            string     `json:"email,omitempty"`
-	Bio              string     `json:"bio,omitempty"`
-	PublicRepos      int     `json:"public_repo,omitempty"`
-	PublicGists      int     `json:"public_gists,omitempty"`
-	Followers        int    `json:"followers,omitempty"`
-	Following        int    `json:"following,omitempty"`
-	CreatedAt        string     `json:"created_at,omitempty"`
-	UpdatedAt        string     `json:"updated_at,omitempty"`
+	NodeID           string     `json:"node_id"`
+	GravatarID	     string 	 `json:"gravatar_id"`
+	AvatarURL        string     `json:"avatar_url"`
+	TwitterUsername  string		`json:"twitter_username"`
+	Name 		     string 	 `json:"name"`
+	Company 	     string 	 `json:"company"`
+	Blog             string     `json:"blog"`
+	Location         string     `json:"location"`
+	Email            string     `json:"email"`
+	Bio              string     `json:"bio"`
+	PublicRepos      int     `json:"public_repos"`
+	PublicGists      int     `json:"public_gists"`
+	Followers        int    `json:"followers"`
+	Following        int    `json:"following"`
+	CreatedAt        string     `json:"created_at"`
+	UpdatedAt        string     `json:"updated_at"`
 }
 
 type GitHubFollowers struct {
-	Login 	        string 		`json:"login,omitempty"`
+	Login 	        string 		`json:"login"`
 }
 
 
@@ -78,70 +78,80 @@ func FindMutualFollowers(followers []GitHubFollowers, username string) ([]string
 
 
 func DisplayMutualFollowers(mutualFollowers []string) {
+	
+	fmt.Println()
+	
 	if len(mutualFollowers) > 10 {
 		top10 := mutualFollowers[:10]
 
-		fmt.Println("[+] Top 10 Mutual Followers:")
+
+		Green("[+] Top 10 Mutual Followers:").Println()
 
 		for i := range(top10) {
-			fmt.Printf("[+] ↳ %s", i)
+			Greenf("[+] ↳ %s", i).Println()
 		}
-	} else {
-		fmt.Println("[+] Mutual Followers:")
+	} 
+	
+	if len(mutualFollowers) > 0 {
+		Green("[+] Mutual Followers:").Println()
 
 		for i := range(mutualFollowers) {
-			fmt.Printf("[+] ↳ %s", i)
+			Greenf("[+] ↳ %s", i).Println()
 		}
+	} else {
+		return
 	}
 }
 
 func DisplayGitHubInfo(githubUser GitHubUser, username string) {
 
-	Greenf("[+] GitHub username found: %s", username)
-	Greenf("[+] ↳ Created at: %s", githubUser.CreatedAt)
-	Greenf("[+] ↳ Updated at: %s", githubUser.UpdatedAt)
+	fmt.Println()
+
+	Greenf("[+] GitHub username found: %s", username).Println()
+	Greenf("[+] ↳ Created at: %s", githubUser.CreatedAt).Println()
+	Greenf("[+] ↳ Updated at: %s", githubUser.UpdatedAt).Println()
 
 	if githubUser.Name != "" {
-		Greenf("[+] ↳ Name: %s", githubUser.Name)
+		Greenf("[+] ↳ Name: %s", githubUser.Name).Println()
 	}
 
 	if githubUser.Email != "" {
-		Greenf("[+] ↳ Email: %s", githubUser.Email)
+		Greenf("[+] ↳ Email: %s", githubUser.Email).Println()
 	}
 
 	if githubUser.Location != "" {
-		Greenf("[+] ↳ Current location: %s", githubUser.Location)
+		Greenf("[+] ↳ Current location: %s", githubUser.Location).Println()
 	}
 
 	if githubUser.Bio != "" {
-		Greenf("[+] ↳ Bio: %s", githubUser.Bio)
+		Greenf("[+] ↳ Bio: %s", githubUser.Bio).Println()
 	}
 
 	if githubUser.Company != "" {
-		Greenf("[+] ↳ Current company: %s", githubUser.Company)
+		Greenf("[+] ↳ Current company: %s", githubUser.Company).Println()
 	}
 
 	if githubUser.Blog != "" {
-		Greenf("[+] ↳ Blog/personal website: %s", githubUser.Blog)
+		Greenf("[+] ↳ Blog/personal website: %s", githubUser.Blog).Println()
 	}
 
 	if githubUser.GravatarID != "" {
-		Greenf("[+] ↳ Gravatar ID: %s", githubUser.GravatarID)
+		Greenf("[+] ↳ Gravatar ID: %s", githubUser.GravatarID).Println()
 	}
 
 	if githubUser.TwitterUsername != "" {
-		Greenf("[+] ↳ Twitter username: %s", githubUser.TwitterUsername)
+		Greenf("[+] ↳ Twitter username: %s", githubUser.TwitterUsername).Println()
 	}
 
 	if githubUser.AvatarURL != "" {
-		Greenf("[+] ↳ Avatar URL: %s", githubUser.AvatarURL)
+		Greenf("[+] ↳ Avatar URL: %s", githubUser.AvatarURL).Println()
 	}
 
-	Greenf("[+] ↳ Number of public repositories: %s", githubUser.PublicRepos)
-	Greenf("[+] ↳ Number of public gists: %s", githubUser.PublicGists)
+	Greenf("[+] ↳ Number of public repositories: %d", githubUser.PublicRepos).Println()
+	Greenf("[+] ↳ Number of public gists: %d", githubUser.PublicGists).Println()
 
-	Greenf("[+] ↳ Number of followers: %s", githubUser.Followers)
-	Greenf("[+] ↳ Number of people they follow: %s", githubUser.Following)
+	Greenf("[+] ↳ Number of followers: %d", githubUser.Followers).Println()
+	Greenf("[+] ↳ Number of people they follow: %d", githubUser.Following).Println()
 
-	Greenf("[+] ↳ Node ID: %s", githubUser.NodeID)
+	Greenf("[+] ↳ Node ID: %s", githubUser.NodeID).Println()
 }
