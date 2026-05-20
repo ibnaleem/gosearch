d5da38acdbed3aed9828463cb11bbe8d0b427bfe
ibnaleem <ibnaleem@outlook.com>
2026-05-20 17:08:33 +0100

    feat(github): add DisplayEmailsFromCommits

--- internal/modules/github/email-from-commits.go
+++ internal/modules/github/email-from-commits.go
@@ -8,7 +8,35 @@ import (
 	"strings"
 
 	"github.com/ibnaleem/gosearch/internal/config"
+	"github.com/ibnaleem/gosearch/internal/theme"
+	"github.com/ibnaleem/gosearch/internal/utils"
 )
+
+func DisplayEmailsFromCommits(username string) {
+	theme.Yellow("[*] Extracting emails from public commits for ", username, "...").Println()
+
+	events, err := FetchPublicEvents(username)
+	if err != nil {
+		theme.Redf("[-] Error fetching public events: %v", err).Println()
+		return
+	}
+
+	emails := ExtractEmailsFromCommits(events)
+
+	if len(emails) == 0 {
+		theme.Red("[-] No emails found in public commits").Println()
+		utils.WriteToFile(username, "[-] No emails found in public commits\n")
+		return
+	}
+
+	theme.Greenf("[+] Found %d unique email(s) from commits:", len(emails)).Println()
+	utils.WriteToFile(username, fmt.Sprintf("[+] Found %d unique email(s) from commits:\n", len(emails)))
+
+	for email, name := range emails {
+		theme.Greenf("[+] ↳ %s (%s)", email, name).Println()
+		utils.WriteToFile(username, fmt.Sprintf("[+] ↳ %s (%s)\n", email, name))
+	}
+}
 
 // ExtractEmailsFromCommits returns a deduplicated map of email -> author name
 // from PushEvents, filtering out GitHub's noreply privacy addresses.
