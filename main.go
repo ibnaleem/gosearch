635b553ff4158587038808cadd4b776ea380914f
ibnaleem <ibnaleem@outlook.com>
2026-05-20 21:47:03 +0100

    feat(github): call DisplayGPGKeys in main search flow

--- main.go
+++ main.go
@@ -97,6 +97,11 @@ func main() {
 	fmt.Println()
 	fmt.Println()
 
+	github.DisplayGPGKeys(username)
+
+	fmt.Println()
+	fmt.Println()
+
 	wg.Add(1)
 	utils.WriteToFile(username, strings.Repeat("⎯", 85))
 	theme.Yellow("[*] Searching HudsonRock's Cybercrime Intelligence Database...").Println()
